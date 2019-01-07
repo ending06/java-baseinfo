da416103e6122ca87107bb7bce634df697a87b40
ruirui.qu <ruirui.qu@qunar.com>
2019-01-07 17:22:35 +0800

    init

---
+++ src/main/java/com.java.baseinfo.knowledge/code/serializable/Person.java
@@ -0,0 +1,63 @@
+package com.java.baseinfo.knowledge.code.serializable;
+
+import java.io.Serializable;
+
+// --------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2019/1/4<p>
+// -------------------------------------------------------
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = -3534890433624150186L;
+    private int age;
+
+    private String name;
+
+    private String sex;
+
+    // 增加字段测试序列化
+    private String addText;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddText() {
+        return addText;
+    }
+
+    public void setAddText(String addText) {
+        this.addText = addText;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Person{");
+        sb.append("age=").append(age);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", sex='").append(sex).append('\'');
+        sb.append(", addText='").append(addText).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com.java.baseinfo.knowledge/code/serializable/TestObjSerializeAndDeserialize.java
@@ -0,0 +1,70 @@
+package com.java.baseinfo.knowledge.code.serializable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+// --------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2019/1/4<p>
+// -------------------------------------------------------
+
+public class TestObjSerializeAndDeserialize {
+
+    public static void main(String[] args) {
+        // 序列化
+        //SerializablePerson();
+        // 反序列化
+        Deserialization();
+    }
+
+    private static void SerializablePerson() {
+        Person person = new Person();
+        person.setAge(10);
+        person.setName("测试");
+        person.setSex("女");
+
+        String path = "src/test/resources/person.txt";
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(path));
+
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            objectOutputStream.writeObject(person);
+
+            System.out.println("序列化成功");
+            fileOutputStream.close();
+            objectOutputStream.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.printf("FileNotFoundException====>" + e);
+        } catch (IOException e) {
+            System.out.printf("IOException====>" + e);
+        }
+    }
+
+    private static Person Deserialization() {
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File("src/test/resources/person.txt"));
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            Person person = (Person) objectInputStream.readObject();
+
+            System.out.printf("Deserialization.person===>"+person);
+
+        } catch (FileNotFoundException e) {
+            System.out.printf("FileNotFoundException====>" + e);
+        } catch (IOException e) {
+            System.out.printf("IOException====>" + e);
+        } catch (ClassNotFoundException e) {
+            System.out.printf("ClassNotFoundException====>" + e);
+        }
+        return null;
+    }
+}
